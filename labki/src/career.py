9110abc585ef6d39fd16e20aef89a094b7ccd68b
kubadiba <151617774+kubadiba@users.noreply.github.com>
2024-04-19 13:06:23 +0300

    переробила

--- labki/src/career.py
+++ labki/src/career.py
@@ -1,24 +1,27 @@
-def dfs(level, position, experience, hierarchy):
-    if level == len(hierarchy) - 1:
-        return hierarchy[level][position]
+def bfs(hierarchy):
+    max_experience = 0
+    queue = [(0, 0, 0)]
+    front = 0
 
-    left_child = dfs(level + 1, position, experience, hierarchy)
-    right_child = dfs(level + 1, position + 1, experience, hierarchy)
+    while front < len(queue):
+        level, position, experience = queue[front]
+        front += 1
+        if level == len(hierarchy) - 1:
+            max_experience = max(max_experience, experience + hierarchy[level][position])
+        else:
+            queue.append((level + 1, position, experience + hierarchy[level][position]))
+            queue.append(
+                (level + 1, position + 1, experience + hierarchy[level][position])
+            )
+    return max_experience
 
-    if left_child > right_child:
-        return hierarchy[level][position] + left_child
-    else:
-        return hierarchy[level][position] + right_child
 
-def max_experience(hierarchy):
-    return dfs(0, 0, 0, hierarchy)
-
-with open('career.in', 'r') as f:
+with open("career.in", "r") as f:
     L = int(f.readline())
     hierarchy = []
     for _ in range(L):
         hierarchy.append(list(map(int, f.readline().split())))
 
-max_exp = max_experience(hierarchy)
-with open('career.out', 'w') as f:
+max_exp = bfs(hierarchy)
+with open("career.out", "w") as f:
     f.write(str(max_exp))
--- labki/test/career_unittest.py
+++ labki/test/career_unittest.py
@@ -4,13 +4,25 @@ from src.career import max_experience
 
 class TestMaxExperience(unittest.TestCase):
     def test_max_experience(self):
-        hierarchy = [[4], [3, 1], [2, 1, 5], [1, 3, 2, 1]]
-        expected_max_experience = 12
+        # Test case 1
+        hierarchy1 = [
+            [3],
+            [7, 4],
+            [2, 4, 6],
+            [8, 5, 9, 3]
+        ]
+        self.assertEqual(max_experience(hierarchy1), 23)
 
-        actual_max_experience = max_experience(hierarchy)
+        # Test case 2
+        hierarchy2 = [
+            [1],
+            [2, 3],
+            [4, 5, 6],
+            [7, 8, 9, 10]
+        ]
+        self.assertEqual(max_experience(hierarchy2), 22)
 
-        self.assertEqual(actual_max_experience, expected_max_experience)
+        # Additional test cases can be added here
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
